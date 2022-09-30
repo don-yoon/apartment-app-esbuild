bb97fd8487d31cf3d30ee77d4ec2234e7018e693
Don <dongho.yoon@gmail.com>
2022-09-30 17:09:40 -0500

    update app environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "apartment_app_esbuild_2_production"
+  # config.active_job.queue_name_prefix = "apartment_app_esbuild2_production"
 
   config.action_mailer.perform_caching = false
 
